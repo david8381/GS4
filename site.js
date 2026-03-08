e4c795a00f333ac93081a7bb7a5bad7fc4341c20
David Daniels <daviddaniels@Davids-Air.localdomain>
2026-03-08 10:13:32 -0400

    Reduce vertical space in profile form fields

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 (() => {
 const SITE_PROFILE_KEY = "gs4.characterProfiles";
 const SITE_SELECTED_PROFILE_KEY = "gs4.selectedProfileId";
-const SITE_VERSION = "0.1.34";
+const SITE_VERSION = "0.1.35";
 
 function loadProfiles() {
   try {
