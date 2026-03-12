7f0907841de08728ea2310097dcd905011c1a850
David Daniels <daviddaniels@Davids-MacBook-Air.local>
2026-03-11 20:56:06 -0400

    Fix optimizer scenario control layout

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 (() => {
-const SITE_VERSION = "0.2.31";
+const SITE_VERSION = "0.2.32";
 const storage = globalThis.GS4Storage;
 
 if (!storage) throw new Error("GS4Storage is not loaded. Ensure shared.js is loaded before site.js.");
